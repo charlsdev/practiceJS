120683c246fec34256150cc65b30a01bdfbd2aa7
CharlesVP15 <youquince@gmail.com>
2022-11-12 23:01:14 -0500

    Fix errors of challenge for organize schedule

--- Order JSON - Challenge.js
+++ Order JSON - Challenge.js
@@ -1,52 +1,39 @@
 const data = [
   {
-    desiredCourse: 'PortfolioConstruction',
-    requiredCourse: 'PortfolioTheories',
-  },
-  {
-    desiredCourse: 'InvestmentManagement',
-    requiredCourse: 'Investment',
-  },
-  {
-    desiredCourse: 'Investment',
-    requiredCourse: 'Finance',
-  },
-  {
-    desiredCourse: 'PortfolioTheories',
-    requiredCourse: 'Investment',
-  },
-  {
-    desiredCourse: 'InvestmentStyle',
-    requiredCourse: 'InvestmentManagement',
-  },
-  {
-    desiredCourse: 'Finance',
-    requiredCourse: 'Programming',
-  },
+    "desiredCourse": "PortfolioConstruction",
+    "requiredCourse": "PortfolioTheories"
+  }, {
+    "desiredCourse": "InvestmentManagement",
+    "requiredCourse": "Investment"
+  }, {
+    "desiredCourse": "Investment",
+    "requiredCourse": "Finance"
+  }, {
+    "desiredCourse": "PortfolioTheories",
+    "requiredCourse": "Investment"
+  }, {
+    "desiredCourse": "InvestmentStyle",
+    "requiredCourse": "InvestmentManagement"
+  }, {
+    "desiredCourse": "Finance",
+    "requiredCourse": "Programming"
+  }
 ];
 
-const table: [] = [];
+let table: [] = [];
+
+function orderInfo (desiredCourse: string, requiredCourse: string) {
+  if (!table.includes(requiredCourse)) table = [requiredCourse, ...table]
+  if (table.includes(desiredCourse)) {
+    table.splice(table.indexOf(desiredCourse), 1)
+  }
+  
+  table.splice(table.indexOf(requiredCourse) + 1, 0, desiredCourse)
+}
 
 data.forEach((item) => {
-  const { desiredCourse, requiredCourse } = item;
-  
-  if (table.includes(requiredCourse)) {
-    if (table.includes(desiredCourse)) {
-      table.splice(table.indexOf(desiredCourse), 1)
-      table.splice(table.indexOf(requiredCourse) + 1, 0, desiredCourse)
-    } else {
-      table.splice(table.indexOf(requiredCourse) + 1, 0, desiredCourse)
-    }
-  } else {
-    if (table.includes(desiredCourse)) {
-      table.unshift(requiredCourse);
-      table.splice(table.indexOf(desiredCourse), 1)
-      table.splice(table.indexOf(requiredCourse) + 1, 0, desiredCourse)
-    } else {
-      table.unshift(requiredCourse);
-      table.splice(table.indexOf(requiredCourse), 0, desiredCourse)
-    }
-  }
+  const { desiredCourse, requiredCourse } = item;  
+  orderInfo(desiredCourse, requiredCourse)
 });
 
 console.log(table);
